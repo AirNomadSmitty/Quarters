a05ce8f2af0871a304ac5edd811926ccea24d6ed
David Murro <murrod@gmail.com>
2019-08-06 22:18:36 -0500

    Adding auth handler/wrapper

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"github.com/airnomadsmitty/quarters/mappers"
+	"github.com/airnomadsmitty/quarters/utils"
 
 	"github.com/airnomadsmitty/quarters/routes"
 
@@ -41,4 +42,36 @@ func main() {
 
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", nil)
+}
+
+type AuthenticatedHandler func(http.ResponseWriter, *http.Request, *utils.Auth)
+
+type AuthenticatedWrapper struct {
+	handler AuthenticatedHandler
+}
+
+func (wrapper *AuthenticatedWrapper) ServeHTTP(res http.ResponseWriter, req *http.Request) {
+	auth, _ := getAuthFromRequest(req)
+	wrapper.handler(res, req, auth)
+}
+
+func NewAuthenticatedWrapper(handler AuthenticatedHandler) *AuthenticatedWrapper {
+	return &AuthenticatedWrapper{handler}
+}
+
+func getAuthFromRequest(req *http.Request) (*utils.Auth, error) {
+	c, err := req.Cookie("token")
+	noAuth := &utils.Auth{}
+	if err != nil {
+		return noAuth, err
+	}
+
+	tokenString := c.Value
+
+	auth, err := utils.GetAuthFromJWT(tokenString)
+	if err != nil {
+		return noAuth, err
+	}
+
+	return auth, nil
 }
